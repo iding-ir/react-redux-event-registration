adb27e1af1c2f3f74e28916282d71d754b5f149d
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-05-27 20:11:34 +0430

    Code refactor

--- src/components/navigator.js
+++ src/components/navigator.js
@@ -3,40 +3,10 @@ import React from "react";
 import "./navigator.scss";
 
 const Navigator = (props) => {
-  const { form, step, nextStep, prevStep, resetSteps } = props;
-
-  const renderReset = () => {
-    if (step === 2 || step === 3) {
-      return (
-        <div className="reset sc-form-button sc-md">
-          <button type="button" onClick={resetSteps}>
-            <i className="sc-icon-cross"></i>
-
-            <span>Reset</span>
-          </button>
-        </div>
-      );
-    }
-  };
-
-  const renderPrev = () => {
-    if (step === 2 || step === 3) {
-      return (
-        <div className="prev sc-form-button sc-md">
-          <button type="button" onClick={prevStep}>
-            <i className="sc-icon-left"></i>
-
-            <span>Prev</span>
-          </button>
-        </div>
-      );
-    }
-  };
+  const { form, step, nextStep } = props;
 
   const renderNext = () => {
-    if (step === 1 || step === 2) {
-      console.log(form.registration);
-
+    if (step === 1) {
       const disabled = !form.registration || form.registration.syncErrors;
 
       return (
@@ -48,18 +18,20 @@ const Navigator = (props) => {
           </button>
         </div>
       );
+    } else if (step === 2) {
+      return (
+        <div className="next sc-form-button sc-md">
+          <button type="button" onClick={nextStep}>
+            <i className="sc-icon-right"></i>
+
+            <span>Next</span>
+          </button>
+        </div>
+      );
     }
   };
 
-  return (
-    <div className="navigator">
-      {renderReset()}
-
-      {renderPrev()}
-
-      {renderNext()}
-    </div>
-  );
+  return <div className="navigator">{renderNext()}</div>;
 };
 
 export default Navigator;
