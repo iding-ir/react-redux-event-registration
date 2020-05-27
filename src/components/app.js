5162a3088534cde96435e691a3547267ec4d4b42
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-05-27 13:30:42 +0430

    Scaffold project

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import "./app.scss";
+
+class App extends Component {
+  render() {
+    return <div className="app">App</div>;
+  }
+}
+
+export default App;
