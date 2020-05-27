d2b65c56ab7a280cde4d9eec829ba2a8f6e229f2
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-05-28 01:35:35 +0430

    Improve Summary component

--- src/components/summary.js
+++ src/components/summary.js
@@ -17,6 +17,50 @@ const Summary = (props) => {
 
         <div className="list"></div>
       </div>
+
+      <div className="controls">
+        <form className="sc-form">
+          <div className="sc-form-group sc-grid-2">
+            <div className="sc-form-button sc-md">
+              <button type="button">
+                <i className="sc-icon-zoom-in"></i>
+
+                <span>Button with icon</span>
+              </button>
+            </div>
+
+            <div className="total"></div>
+          </div>
+        </form>
+      </div>
+
+      <div className="payment">
+        <h5>Choose Your Payment Method</h5>
+
+        <form className="sc-form">
+          <div className="sc-form-group sc-grid-2">
+            <div className="sc-form-radio">
+              <input type="radio" name="my-radio" id="now" checked />
+
+              <label for="now">
+                <i className="sc-icon-radio"></i>
+
+                <span>Pay Now</span>
+              </label>
+            </div>
+
+            <div className="sc-form-radio">
+              <input type="radio" name="my-radio" id="later" />
+
+              <label for="later">
+                <i className="sc-icon-radio"></i>
+
+                <span>Book Now, Pay Later</span>
+              </label>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
